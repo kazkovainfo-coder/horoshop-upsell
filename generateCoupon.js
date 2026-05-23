485f4196ba2a4b3ff30155964ed3b2059b8aa9ce
kazkovainfo-coder <kazkovainfo@gmail.com>
2026-05-23 04:10:33 +0300

    Update generateCoupon.js

--- generateCoupon.js
+++ generateCoupon.js
@@ -3,7 +3,7 @@ const { chromium } = require("playwright");
 async function generateCoupon() {
 
   const browser = await chromium.launch({
-    headless: false
+    headless: true
   });
 
   const page = await browser.newPage();
